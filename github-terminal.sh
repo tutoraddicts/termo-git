65a6bb0eb5e44dc63d675f14b85f5fcce0ef593b
tutoraddicts <rohitdutta907@gmail.com>
2019-10-21 01:36:53 +0530

    add initialisation

--- github-terminal.sh
+++ github-terminal.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+###############################################################
+##Terminal Programe Git Hub known as TERMO-git
+###############################################################
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+###############################################################
+##Function for globally initialise you EMAIL_ID and USER_NAME
+## read emailID -> Reading email id and storing in emailID variable
+## it config --global user.email emailID -> code foe config email id
+###############################################################
+
 initialise_ids() {
 	echo "Enter your github registered email address"
 	read emailID
@@ -7,13 +17,23 @@ initialise_ids() {
 	read ID
 	git config --global user.name ID
 }
+
+###############################################################
+##Function for initialise the git repo for first time
+## git init -> is for initialise local git repo
+## git add * for updating all the files in the repo
+##Then we use a while look lop to ask you that you want to see the git status or not
+## git commit -e commit -> this command used to initialise commitment of git
+## git remote add origin $repo_URL -> this command and the repo link  the local git repo
+## git push -u origin master -> finally we are uploading or can say pushing the files into the git repo
+###############################################################
+
 first_time() {
 	echo "Initialising"
 	git init
 	echo "Inisialized"
 	echo "Adding All your files"
 	git add *
-	initialise_ids
 	echo "Want to show git status yes or no[y/n]"
 	while :
 	do
@@ -46,16 +66,32 @@ first_time() {
 }
 
 second_time() {
+    git init
 	git add *
 	echo "Enter your commit details"
 	read commit_details
-	git commit -e $commit_deatils
+	git commit -e $commit_details
 	echo "Pushing to branch master"
 	git push -u origin master
 	echo "We are all done"
 }
 
+Identification() {
+username=$(git config user.name)
+emailid=$(git config user.email)
 
+if [ -z "$username" && -z "$emailid"]
+then 
+    echo "Your Git email id and user id are not Present in your computer"
+    echo "So Enter What we asked :) "
+    echo ""
+    initialise_ids
+else
+    main_program
+fi
+}
+
+main_program() {
 echo "Welcome To the git project. There some simple steps you have to do here.So We will guid you through this."
 echo "Press the desired number and press enter"
 echo "1.Upload source to github first time."
@@ -92,3 +128,6 @@ do
 			;;
  esac
 done
+}
+
+Identification
